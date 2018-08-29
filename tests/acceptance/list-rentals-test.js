9da4222c3ae42bdcc8c42c0d796790416f3165ce
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-08-29 12:53:15 -0700

    write tests for image toggle and displaying rental details

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,10 +1,12 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, click } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import { visit, currentURL, click, render } from '@ember/test-helpers';
+import { setupApplicationTest, setupRenderingTest } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
-
+  
   test('should show rentals as the home page', async function(assert) {
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
@@ -30,8 +32,34 @@ module('Acceptance | list rentals', function(hooks) {
   test('should filter the list of rentals by city', async function(assert) {
     assert
   });
+});
+
+module('Integration | Component | list rentals', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.rental = EmberObject.create({
+      image: 'fake.png',
+      title: 'test-title',
+      owner:'test-owner',
+      type: 'test-type',
+      city: 'test-city',
+      bedrooms: 3
+    });
+  });
 
   test('should show details for a selected rental', async function(assert) {
-    assert
+    await render(hbs`{{rental-listing rental=rental}}`);
+    assert.equal(this.element.querySelector('.listing h3').textContent.trim(), 'test-title', 'Title: test-title');
+    assert.equal(this.element.querySelector('.listing .owner').textContent.trim(), 'Owner: test-owner', 'Owner: test-owner');
+  });
+
+  test('should toggle wide class on click', async function(assert) {
+    await render(hbs`{{rental-listing rental=rental}}`);
+    assert.notOk(this.element.querySelector('.image.wide'), 'initially rendered small');
+    await click('.image');
+    assert.ok(this.element.querySelector('.image.wide'), 'rendered wide after click');
+    await click('.image');
+    assert.notOk(this.element.querySelector('.image.wide'), 'rendered small after second click');
   });
 });
