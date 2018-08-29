7c0b48d26b853be494b648bc68606140e4180f37
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-08-29 13:22:18 -0700

    create a model for rental data and remove hard-coded data from rentals routes

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage'
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
   
   test('should show rentals as the home page', async function(assert) {
     await visit('/');
